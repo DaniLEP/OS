6117d5f688870855778a809e6a5bf70a9b565fba
DaniLEP <danilomanzoli64@gmail.com>
2024-11-05 15:46:55 -0300

    fix: add meses que ficaram faltando

--- src/componentes/UploadControl.jsx
+++ src/componentes/UploadControl.jsx
@@ -214,8 +214,19 @@ const DataForm = () => {
           style={styles.input}
         >
           <option value="">Selecione o mês</option>
+          <option value="">Selecione o mês</option>
           <option value="Janeiro">Janeiro</option>
           <option value="Fevereiro">Fevereiro</option>
+          <option value="Março">Março</option>
+          <option value="Abril">Abril</option>
+          <option value="Maio">Maio</option>
+          <option value="Junho">Junho</option>
+          <option value="Julho">Julho</option>
+          <option value="Agosto">Agosto</option>
+          <option value="Setembro">Setembro</option>
+          <option value="Outubro">Outubro</option>
+          <option value="Novembro">Novembro</option>
+          <option value="Dezembro">Dezembro</option>
           {/* demais opções de meses */}
         </select>
         <input
